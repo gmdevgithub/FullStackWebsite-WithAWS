bd67368fac6828f2cb80a9d4198c1a5200e8f452
unknown <Yupimaperson101@protonmail.com>
2020-09-19 22:27:47 -0500

    Website can now communicate with DB through axios calls

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,6 +1,7 @@
 
 //Basic imports
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 import logo from '../assets/logo.svg';
 import '../css/App.css';
 import '../css/Crack.css';
@@ -10,12 +11,65 @@ import Gio from "../assets/Gio.png"
 
 //Imports for bootstrap css styling 
 import "bootstrap/dist/css/bootstrap.min.css";
+import { DesktopWindowsRounded } from '@material-ui/icons';
 
 
 
 
 
-const Users = () => {
+export default class Users extends Component {
+
+
+    //Convert to Hook later 
+    constructor(props) 
+    {
+        super(props); 
+        
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+            this.state = {
+                username: ' ',
+        
+               
+            }
+        
+    }
+
+   
+    //Change to hook
+    onChangeUsername(e)
+    {
+        this.setState({
+
+            username: e.target.value
+        });
+    }
+
+
+       //Change to hook
+    onSubmit(e)
+    {
+        e.preventDefault();
+
+        const user = {
+            username: this.state.username
+        }
+
+        console.log(user);
+        console.log("User Submitted");
+
+        axios.post('http://localhost:5000/users/add', user)
+        .then(res => console.log(res.data))
+        
+
+        this.setState({
+            username: ' '
+        })
+
+    }
+
+    render(){
 
     return (
 
@@ -25,10 +79,32 @@ const Users = () => {
             <header className="App-header">
                      <img src={Gio} className="App-logo" alt="logo" />
             </header>
+
+            <div>
+                <h3> Create new user</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className = "form-group">
+                        <label>Username: </label>
+                        <input type="text"
+                            required
+                            className="form-control"
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}
+                                
+                            />
+                    </div>
+                    <div className = "form-group">
+                        <input type ="submit" value="Create User log" className="btn btn-primary">
+
+                        </input>
+                    </div>
+                    
+                </form>
+            </div>
         </div>
         
     )
+    }
 
 };
 
-export default Users;
