8db90120f7631f177638d839d0a57769583f6a33
Msal44 <mlvynslvtrr000@gmail.com>
2020-11-30 23:47:56 -0600

    Are you coding son?

--- backendAWS/AWSconnection.js
+++ backendAWS/AWSconnection.js
@@ -1,21 +1,19 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host     : process.env.RDS_Amazon,
-    user     : process.env.RDS_Admin,
-    password : process.env.RDS_Jerry0511,
-    port     : process.env.RDS_5001
+  host     : process.env.RDS_HOSTNAME = "db-niu.cv418tv6s0pc.us-east-2.rds.amazonaws.com",
+  user     : process.env.RDS_USERNAME = "NIUadmin",
+  password : process.env.RDS_PASSWORD = "467niucsci",
+  port     : process.env.RDS_PORT      = "5001"
 });
 
 connection.connect(function(err) {
-    
-    if(err){
-        console.error('Database connection failed' + err.stack) 
-        return;
-    }
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
 
- console.log('Connected to database.');
+  console.log('Connected to database.');
 });
 
 connection.end();
-
